dbe8e710efa0bae0779ab749a3d8b0423c98165f
FATIMA ASEBBANE <126819418+fatimaasebbane@users.noreply.github.com>
2024-05-13 20:26:32 +0200

    résoudre les conflits de SensorDataDTO

--- src/main/java/com/codeverce/aquasonicbackend/DTO/SensorDataDTO.java
+++ src/main/java/com/codeverce/aquasonicbackend/DTO/SensorDataDTO.java
@@ -1,7 +1,5 @@
 package com.codeverce.aquasonicbackend.DTO;
-
 import lombok.Data;
-
 
 @Data
 public class SensorDataDTO {
@@ -9,12 +7,4 @@ public class SensorDataDTO {
     private int leak;
     private String time;
     private double[] amplitudes;
-<<<<<<< HEAD
-    public int getLeak() {
-        return this.leak;
-    }
-=======
->>>>>>> 756c04ebadff9c2eac12169af9b3708ada3657cb
-
-
 }
