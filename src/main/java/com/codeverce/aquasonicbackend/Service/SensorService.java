522c5a4f7455df2302c78c6d8ff4a8092d5916b4
FATIMA ASEBBANE <126819418+fatimaasebbane@users.noreply.github.com>
2024-05-13 21:10:19 +0200

    Update SensorService.java

--- src/main/java/com/codeverce/aquasonicbackend/Service/SensorService.java
+++ src/main/java/com/codeverce/aquasonicbackend/Service/SensorService.java
@@ -137,67 +137,16 @@ public class SensorService {
         // Interroger la base de données pour obtenir les données entre ces deux dates
         return sensorDataRepository.findByDateBetweenAndSensorId(sensor_id,startDateString, endDateString);
     }
-  
-    public Map<String, List<SensorData>> getAllSensorsDataForLastTwoDays(){
+
+    public Map<String, List<SensorData>> getAllSensorsDataForLastTwoDays() {
         List<SensorData> AllSensor = sensorDataRepository.findAll();
         Map<String, List<SensorData>> MapSensorsData = new HashMap<>();
-        for (SensorData sensor:AllSensor) {
-           List<SensorData> sensorData = getSensorDataForLastTwoDays(sensor.getSensor_id());
-            MapSensorsData.put(sensor.getSensor_id(),sensorData);
+        for (SensorData sensor : AllSensor) {
+            List<SensorData> sensorData = getSensorDataForLastTwoDays(sensor.getSensor_id());
+            MapSensorsData.put(sensor.getSensor_id(), sensorData);
         }
         return MapSensorsData;
-
-
-    // Méthode pour calculer la moyenne des tensions d'un appel
-    private double calculateAverageTension(double[] tensions) {
-        double sum = 0.0;
-        for (double tension : tensions) {
-            sum += tension;
-        }
-        return sum / tensions.length;
     }
-
-    public Map<String, Double> AllSensorDegreeGravity(){
-        List<SensorData> AllSensor = sensorDataRepository.findAll();
-        Map<String, Double> SensorsGravity = new HashMap<>();
-        for (SensorData sensor:AllSensor) {
-            double gravity= calculateSensorLeakGravity(sensor.getSensor_id());
-            SensorsGravity.put(sensor.getSensor_id(),gravity);
-        }
-        return SensorsGravity;
-    }
-
-
-
-
-    public List<SensorData> getSensorDataForLastTwoDays(String sensor_id){
-       // Date actuelle
-        LocalDate currentDate = LocalDate.now();
-
-        // Date il y a deux jours
-        LocalDate twoDaysAgo = currentDate.minusDays(2);
-
-        // Formatter pour la date
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
-        // Convertir les dates en chaînes de caractères
-        String startDateString = twoDaysAgo.format(formatter);
-        String endDateString = currentDate.format(formatter);
-
-        // Interroger la base de données pour obtenir les données entre ces deux dates
-        return sensorDataRepository.findByDateBetweenAndSensorId(sensor_id,startDateString, endDateString);
-    }
-      
-    public Map<String, List<SensorData>> getAllSensorsDataForLastTwoDays(){
-        List<SensorData> AllSensor = sensorDataRepository.findAll();
-        Map<String, List<SensorData>> MapSensorsData = new HashMap<>();
-        for (SensorData sensor:AllSensor) {
-           List<SensorData> sensorData = getSensorDataForLastTwoDays(sensor.getSensor_id());
-            MapSensorsData.put(sensor.getSensor_id(),sensorData);
-        }
-        return MapSensorsData;
-     }
-
 
     public List<SensorData> getSensorDataBySensorId(String sensor_id){
        return sensorDataRepository.findBySensorId(sensor_id);
