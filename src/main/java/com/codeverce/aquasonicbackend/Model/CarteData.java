a56e551f1c5ce6238cd5118ed2fbd527272d6053
FATIMA ASEBBANE <126819418+fatimaasebbane@users.noreply.github.com>
2024-05-13 21:11:28 +0200

    Update CarteData.java

--- src/main/java/com/codeverce/aquasonicbackend/Model/CarteData.java
+++ src/main/java/com/codeverce/aquasonicbackend/Model/CarteData.java
@@ -8,41 +8,14 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "rapport")
 @Data
 public class  CarteData {
-    public String getId() {
-        return id;
-    }
-
     @Id
-    private String id;
-
-    public String getSensor_id() {
-        return sensor_id;
-    }
-
-    private String sensor_id; // Identifier for the sensor
-
-    public double getX() {
-        return X;
-    }
-
+    private String sensor_id;
     private  double X ;
-
-    public double getY() {
-        return Y;
-    }
-
     private  double Y ;
-
-
-
-     String droite_id ;
-
-    String gauche_id ;
-
-
-     int nb_fuite ;
-
-      int  nb_reparation ;
+    private String droite_id ;
+    private String gauche_id ;
+    private int nb_fuite ;
+    private int  nb_reparation ;
 
 
 }
