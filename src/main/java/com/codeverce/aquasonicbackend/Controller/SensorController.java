368370555b1b1c1fa1ece4516e299fc901c1ab01
Youness SERRAKHI <youness.serrakhi@edu.uca.ma>
2024-05-15 16:58:20 +0100

    refactorer , et ajouter la possibilite de recuperer les donnes selon une date precit

--- src/main/java/com/codeverce/aquasonicbackend/Controller/SensorController.java
+++ src/main/java/com/codeverce/aquasonicbackend/Controller/SensorController.java
@@ -5,6 +5,10 @@ import com.codeverce.aquasonicbackend.Model.SensorData;
 import com.codeverce.aquasonicbackend.Service.SensorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -16,6 +20,7 @@ public class SensorController {
 
 
     private final SensorService sensorService;
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
     public SensorController(SensorService sensorService) {
         this.sensorService = sensorService;
@@ -24,6 +29,17 @@ public class SensorController {
     @GetMapping("/SensorData/{sensor_id}")
     public List<SensorData> getListDataBySensorId(@PathVariable String sensor_id) {
         return sensorService.getAllSensorData(sensor_id);
+    }
+
+    @GetMapping("/SensorDataByDate/{sensor_id}/{dateStr}")
+    public List<SensorDataDTO> getListData(@PathVariable String sensor_id,@PathVariable String dateStr) {
+        Date date = null;
+        try {
+            date = dateFormat.parse(dateStr);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return sensorService.getSensorData(sensor_id, date);
     }
 
     @GetMapping("/SensorDataForToday/{sensor_id}")
